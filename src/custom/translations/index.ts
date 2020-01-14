4ed6cbdb572054bc3443f39a812fc7a709727cc4
Guiller <ghminvest@gmail.com>
2020-01-14 13:13:32 -0300

    Update index.ts

--- src/custom/translations/index.ts
+++ src/custom/translations/index.ts
@@ -1,14 +1,20 @@
 import { en } from '../../translations/en';
 import { ru } from './ru';
+import { ptbr } from './pt-br';
 
 // tslint:disable:no-submodule-imports
+import localePtBr from 'react-intl/locale-data/pt';
 import localeRu from 'react-intl/locale-data/ru';
+
 // tslint:enable
 
-export const customLocaleData = ([...localeRu]);
+export const customLocaleData = ([...localeRu, localePtBr]);
 
 export type LangType = typeof en;
 
 export const customLanguageMap = {
     ru,
+    ptbr
 };
+
+
